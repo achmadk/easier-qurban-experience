6e3901ee4e64fdafad2d24f4883e304112af9675
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2022-07-09 05:43:11 +0700

    fix(admin home page): :ambulance: fix error reading `user` object value
    add optional chaining operator to get `user` object

--- src/pages/admin/home.tsx
+++ src/pages/admin/home.tsx
@@ -3,6 +3,6 @@ import { useSession } from "next-auth/react"
 export default function AdminHome() {
   const { data } = useSession()
   return (
-    <h1>`Welcome ${data.user?.name ?? '-'}`</h1>
+    <h1>`Welcome ${data?.user?.name ?? '-'}`</h1>
   )
 }
